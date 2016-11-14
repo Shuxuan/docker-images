7089c2b4a6b941fca4c2b3643bf756725c891451
oracle <oracle@docker-node1.shuxuandomain>
2016-11-14 14:31:04 -0500

    commit jdk8 and weblogic

---
+++ 12.2.1.2-generic-install/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker build -t weblogic:12.2.1.2-generic .
+#docker build -t weblogic:12.2.1.2-generic .
---
+++ 12.2.1.2-generic-install/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#docker run -d --name=wlsadmin -p 8001:8001 weblogic:12.2.1.2-generic
+docker run --name=wlsadmin -p 8001:8001 weblogic:12.2.1.2-generic
